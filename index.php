62ad0531e079f7bcf94fd4811a9502afcf9f06d1
froginfrog <wrx_1984@163.com>
2018-04-14 16:58:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if(version_compare(PHP_VERSION,'5.5.0','<'))  die('要求php版本至少为5.5.0
 
 define("APP_ROOT", __DIR__);//网站根目录
 
-define('URL_ROOT', 'binbin'); //网站所在目录如果不是根目录填写目录名，如果是根目录为空
+define('URL_ROOT', ''); //网站所在目录如果不是根目录填写目录名，如果是根目录为空
 
 date_default_timezone_set("Asia/Shanghai");//设置默认时区
 
